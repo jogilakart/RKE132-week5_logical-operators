66818895b69f709a502dbccad7a257224af331fe
Kärt <jogilakart33@gmail.com>
2023-10-20 21:22:09 +0300

    add week5 code

---
+++ TestPinKood/Program.cs
@@ -0,0 +1,23 @@
+﻿string userPIN, userName, password;
+Console.WriteLine("Enter PIN:");
+userPIN = Console.ReadLine();
+Console.WriteLine("Enter your username:");
+userName = Console.ReadLine();
+Console.WriteLine("Enter your password:");
+password = Console.ReadLine();
+
+if (userPIN == "1234")
+{
+    if(userName == "admin1" && password == "admin1234")
+    {
+    Console.WriteLine("Welcome!");
+    }
+    else
+    {
+    Console.WriteLine("Invalid login credentials.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid PIN");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+
+//OR - || (pipes) 
+
+//true || true --> true
+//true || false --> true
+//flase || true --> true
+
